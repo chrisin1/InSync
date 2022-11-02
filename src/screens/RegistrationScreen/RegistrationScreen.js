89e28ddfb4c14f7ef69721060c2e08a8bf62a222
Nicole Liang <55326415+nicolenliang@users.noreply.github.com>
2022-11-02 05:30:46 -0400

    Code style issues

--- src/screens/RegistrationScreen/RegistrationScreen.js
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -20,56 +20,60 @@ export default function RegistrationScreen({navigation}) {
         <View style={styles.container}>
             <KeyboardAwareScrollView
                 style={{ width: '100%' }}
-                keyboardShouldPersistTaps="always">
+                keyboardShouldPersistTaps='always'>
+                <Text style={styles.title}> Sign up </Text>
                 <Image
                     style={styles.logo}
-                    source={require('../../../assets/music_note_icon.jpg')}
+                    source={require('../../../assets/placeholder-logo.jpg')}
                 />
                 <TextInput
                     style={styles.input}
                     placeholder='Full Name'
-                    placeholderTextColor="#aaaaaa"
+                    placeholderTextColor='white'
                     onChangeText={(text) => setFullName(text)}
                     value={fullName}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
                 />
                 <TextInput
                     style={styles.input}
                     placeholder='E-mail'
-                    placeholderTextColor="#aaaaaa"
+                    placeholderTextColor='white'
                     onChangeText={(text) => setEmail(text)}
                     value={email}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
                 />
                 <TextInput
                     style={styles.input}
-                    placeholderTextColor="#aaaaaa"
-                    secureTextEntry
                     placeholder='Password'
+                    placeholderTextColor='white'
+                    secureTextEntry
                     onChangeText={(text) => setPassword(text)}
                     value={password}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
                 />
                 <TextInput
                     style={styles.input}
-                    placeholderTextColor="#aaaaaa"
-                    secureTextEntry
                     placeholder='Confirm Password'
+                    placeholderTextColor='white'
+                    secureTextEntry
                     onChangeText={(text) => setConfirmPassword(text)}
                     value={confirmPassword}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
                 />
                 <TouchableOpacity
                     style={styles.button}
                     onPress={() => onRegisterPress()}>
-                    <Text style={styles.buttonTitle}>Create account</Text>
+                    <Text style={styles.buttonTitle}>Sign up</Text>
                 </TouchableOpacity>
                 <View style={styles.footerView}>
-                    <Text style={styles.footerText}>Already got an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Log in</Text></Text>
+                    <Text style={styles.footerText}>
+                        Already have an account? 
+                        <Text onPress={onFooterLinkPress} style={styles.footerLink}>  Log in</Text>
+                    </Text>
                 </View>
             </KeyboardAwareScrollView>
         </View>
