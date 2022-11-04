bb7a7f41222d904a3192ee4e107bc1064fc61798
Nicole Liang <55326415+nicolenliang@users.noreply.github.com>
2022-11-04 07:03:56 -0400

    Fixed Bottom Tab Navigation
    Only shows up once user is logged in; only displays chat, home, profile tabs

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
@@ -46,8 +46,6 @@ export default function App() {
       }
     });
   }, []);
-
-
 
   const authContext = useMemo( () => ({
     logIn: async data => {
@@ -112,7 +110,6 @@ export default function App() {
   []  
   );
 
-
   if (loading) {
     return (
       <></>
@@ -123,20 +120,25 @@ export default function App() {
     <AuthContext.Provider value={authContext}>
       <NavigationContainer
         theme={Theme}>
-        <Tab.Navigator
+        { user ? (
+          <Tab.Navigator
+            initialRouteName={"Home"}
             screenOptions={{
-              tabBarStyle: { backgroundColor: '#373737', borderTopWidth: 0 }
+              tabBarStyle: { backgroundColor: '#373737', borderBottomWidth: 0, borderTopWidth: 0 }
             }}>
-          { user ? (
-            <Tab.Screen name="Home" component={HomeScreen} />
-          ) : (
-            <>
-              <Tab.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
-              <Tab.Screen name="Registration" component={RegistrationScreen} options={{ headerShown: false }}/>
-              <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
-            </>
-          )}
-        </Tab.Navigator>
+              <Tab.Screen name="Chat" component={ChatScreen} options={{ headerShown: false }} />
+              <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+              <Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+          </Tab.Navigator>
+        ) : (
+          <Stack.Navigator
+            screenOptions={{
+              cardStyle: { backgroundColor: '#222222' }
+            }}>
+            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
+            <Stack.Screen name="Registration" component={RegistrationScreen} options={{ headerShown: false }}/>
+          </Stack.Navigator>
+        )}
       </NavigationContainer>
     </AuthContext.Provider>
   );
