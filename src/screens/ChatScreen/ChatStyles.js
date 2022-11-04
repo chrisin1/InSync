1b28078dd25dd5f292b0a49e7d709fc0e2399fc0
Arthur Liu <58526720+ardowar@users.noreply.github.com>
2022-11-04 00:17:23 -0400

    Chat screen
    sample chat screen

--- src/screens/ChatScreen/ChatStyles.js
+++ src/screens/ChatScreen/ChatStyles.js
@@ -5,62 +5,40 @@ export default StyleSheet.create({
         alignItems: 'center',
         alignSelf: 'center',
         flex: 1,
+        
     },
     title: {
-        alignSelf: 'center',
+        alignSelf: 'flex-start',
         color: 'white',
         fontSize: 48,
         fontWeight: 'bold',
         margin: 50
     },
-    logo: {
-        alignSelf: 'center',
-        borderRadius: 100,
-        height: 200,
-        marginBottom: 50,
-        width: 200
+    image: {
+        height: 60,
+        width: 60,
+        borderRadius: 30,
+        marginRight: 10
     },
-    input: {
-        backgroundColor: '#373737',
-        borderRadius: 100,
-        color: 'white',
-        height: 48,
-        marginBottom: 20,
-        marginLeft: 50,
-        marginRight: 50,
-        marginTop: 10,
-        outlineStyle: 'none',
-        overflow: 'hidden',
-        paddingLeft: 20, // input text padding
-        width: 500,
+    
+    name: {
+        fontWeight: 'bold',
+        color: 'white'
     },
-    button: {
-        alignItems: 'center',
-        alignSelf: 'center',
-        backgroundColor: '#FF9283',
-        borderRadius: 100,
-        height: 48,
-        justifyContent: 'center',
-        marginTop: 50,
-        width: 150,
+    text: {
+        color: 'white'
     },
-    buttonTitle: {
-        color: 'black',
-        fontSize: 20,
-        fontWeight: 'bold'
+    rightContainer: {
+        width: 300
+        
     },
-    footerView: {
-        alignItems: 'center',
-        flex: 1,
-        marginTop: 10
+    chatContainer: {
+        flexDirection: 'row',
+        padding: 10,
     },
-    footerText: {
-        color: 'white',
-        fontSize: 14
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
-    footerLink: {
-        color: '#FF9283',
-        fontSize: 14,
-        fontWeight: 'bold'
-    }
+   
 })
