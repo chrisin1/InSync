b38466debbe428a280b9f0ef0204a3a784d9b831
Nicole Liang <55326415+nicolenliang@users.noreply.github.com>
2022-11-10 03:55:02 -0500

    Create Setup screen

---
+++ src/screens/SetupScreen/SetupScreen.js
@@ -0,0 +1,100 @@
+import React, { useState, useContext } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { auth, db } from '../../firebase/config'
+import { doc, updateDoc } from 'firebase/firestore'
+import { AuthContext } from '../../AuthContext/AuthContext'
+import styles from './SetupStyles'
+
+export default function SetupScreen({navigation}) {
+    const [displayName, setDisplayName] = useState('')
+    const [profilePic, setProfilePic] = useState('')
+    const [bio, setBio] = useState('')
+    const [age, setAge] = useState('')
+    const [gender, setGender] = useState('')
+    const [location, setLocation] = useState('')
+
+    const onSetupPress = () => {
+        auth.onAuthStateChanged(user => {
+            if (user) { // user is signed in
+                const docRef = doc(db, "users", user.uid);
+                console.log('uid: ', user.uid);
+                updateDoc(docRef, 
+                    {displayName: displayName, bio: bio, age: age, gender: gender, location: location});
+                }
+            });
+        navigation.navigate('Home');
+    }
+
+    // TODO: save inputted info to user
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ width: '100%' }}
+                keyboardShouldPersistTaps='always'>
+                <Text style={styles.title}> Profile </Text>
+                <Image
+                    style={styles.profilePic}
+                    source={require('../../../assets/placeholder-logo.jpg')}
+                />
+                <Text style={[styles.inputTitle, styles.profileUpload]}>Upload Photo</Text>
+                <Text style={styles.inputTitle}>Display Name</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text) => setDisplayName(text)}
+                    value={displayName}
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
+                />
+                <Text style={styles.inputTitle}>Bio</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text) => setBio(text)}
+                    value={bio}
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
+                />
+                <View style={{ flexDirection: 'row', width: 525 }}>
+                    <View style={{ flexDirection: 'column', flex: 1 }}>
+                        <Text style={styles.inputTitle}>Age</Text>
+                        <TextInput
+                            style={[styles.input, { width: 150 }]}
+                            keyboardType='numeric'
+                            onChangeText={(text) => setAge(text)}
+                            value={age}
+                            underlineColorAndroid='transparent'
+                            autoCapitalize='none'
+                        />
+                    </View>
+                    <View style={{ flexDirection: 'column', flex: 1 }}>
+                        <Text style={styles.inputTitle}>Gender</Text>
+                        <TextInput
+                            style={[styles.input, { width: 150 }]}
+                            onChangeText={(text) => setGender(text)}
+                            value={gender}
+                            underlineColorAndroid='transparent'
+                            autoCapitalize='none'
+                        />
+                    </View>
+                    <View style={{ flexDirection: 'column', flex: 1 }}>
+                        <Text style={styles.inputTitle}>Location</Text>
+                        <TextInput
+                            style={[styles.input, { width: 150 }]}
+                            textContentType='addressState'
+                            onChangeText={(text) => setLocation(text)}
+                            value={location}
+                            underlineColorAndroid='transparent'
+                            autoCapitalize='none'
+                        />
+                    </View>
+                </View>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onSetupPress()}>
+                    <Text style={styles.buttonTitle}>Save Profile</Text>
+                </TouchableOpacity>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}
---
+++ src/screens/SetupScreen/SetupStyles.js
@@ -0,0 +1,77 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        flex: 1,
+    },
+    title: {
+        alignSelf: 'center',
+        color: 'white',
+        fontSize: 48,
+        fontWeight: 'bold',
+        margin: 50
+    },
+    profilePic: {
+        alignSelf: 'center',
+        borderRadius: 100,
+        height: 200,
+        marginBottom: 50,
+        width: 200
+    },
+    profileUpload: {
+        alignSelf: 'center',
+        marginLeft: 0,
+        marginTop: -40,
+        marginBottom: 50,
+    },
+    input: {
+        backgroundColor: '#373737',
+        borderRadius: 100,
+        color: 'white',
+        height: 48,
+        marginBottom: 20,
+        marginHorizontal: 50,
+        marginTop: 5,
+        outlineStyle: 'none',
+        overflow: 'hidden',
+        paddingLeft: 20, // input text padding
+        width: 500,
+    },
+    inputTitle: {
+        color: 'white',
+        fontSize: 14,
+        fontWeight: 'bold',
+        marginLeft: 55,
+    },
+    button: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        backgroundColor: '#FF9283',
+        borderRadius: 100,
+        height: 48,
+        justifyContent: 'center',
+        marginTop: 50,
+        width: 150,
+    },
+    buttonTitle: {
+        color: 'black',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    footerView: {
+        alignItems: 'center',
+        flex: 1,
+        marginTop: 10
+    },
+    footerText: {
+        color: 'white',
+        fontSize: 14
+    },
+    footerLink: {
+        color: '#FF9283',
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
+})
